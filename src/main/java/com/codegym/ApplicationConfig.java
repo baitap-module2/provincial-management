c17e764c7b72b25294219cb46a7288757d37efb0
vietson6103@gmail.com <Hn0912388240>
2018-08-27 17:20:25 +0700

    configuration settings (2)

--- src/main/java/com/codegym/ApplicationConfig.java
+++ src/main/java/com/codegym/ApplicationConfig.java
@@ -1,6 +1,8 @@
 package com.codegym;
 
 
+import com.codegym.service.CustomerService;
+import com.codegym.service.CustomerServiceImpl;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
@@ -8,6 +10,7 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -31,6 +34,7 @@ import java.util.Properties;
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
+@EnableJpaRepositories("com.codegym.repository")
 @ComponentScan("com.codegym")
 public class ApplicationConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
@@ -41,6 +45,10 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         this.applicationContext = applicationContext;
     }
 
+    @Bean
+    public CustomerService customerService(){
+        return new CustomerServiceImpl();
+    }
 
     @Bean
     public SpringResourceTemplateResolver templateResolver(){
